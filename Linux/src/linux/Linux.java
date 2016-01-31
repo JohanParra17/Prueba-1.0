22359df5c98d9f551934eaf3cf4383bc011cfc2d
Johan Sebastian Parra Rojas <jsparra880@misnea.edu.co>
2016-01-31 14:11:53 -0500

    Linux 2.0

--- Linux/src/linux/Linux.java
+++ Linux/src/linux/Linux.java
@@ -15,7 +15,7 @@ public class Linux {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Linux Version 1.0");
+        System.out.println("Linux Version 2.0");
     }
     
 }
